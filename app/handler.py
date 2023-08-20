90db6d8c2884507ded32b7f93440b8a4e10ff7ad
Raghav Sharma <71115825+raghavTinker@users.noreply.github.com>
2023-08-21 03:45:38 +0530

    fix

--- app/handler.py
+++ app/handler.py
@@ -1,3 +1,6 @@
 import requests
+
+webhook_url = "https://watchdog.servatom.com/add/activity"
 def main(event, context):
+    requests.post(webhook_url, data = event)
     print(f"IAM Role change detected: {event}")
